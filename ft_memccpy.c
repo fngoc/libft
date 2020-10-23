6f8a7cc9796fca747c5a8fef8170a842853d7c80
VitMarKha <vitmarkha@icloud.com>
2020-10-23 11:54:14 +0700

    Add functions

---
+++ ft_memccpy.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+/*
+** ft_memccpy: копирует данные из массива, на который
+** указывает аргумент src, в массив, на который указывает
+** аргумент dst пока не встретится символ c, или пока не
+** будет скопировано n байт данных.
+*/
+
+void *ft_memccpy(void *restrict dst, const void *restrict src, int c, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (((unsigned char *)src)[i] == (unsigned char)c)
+		{
+			((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+			return (NULL);
+		}
+		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+		++i;
+	}
+	return (dst);
+}
---
+++ ft_memcpy.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+/*
+** ft_memcpy: memcpy копирует n байтов первого блока памяти,
+** на который ссылается указатель src, во второй блок памяти,
+** на который ссылается указатель dst.
+*/
+
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+		++i;
+	}
+	return (dst);
+}
